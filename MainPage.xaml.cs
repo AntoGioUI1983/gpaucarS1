4487f03f280961787f6dfb409415664d582206ae
GIO <GIO@DESKTOP-BQDO65U>
2024-04-07 19:19:50 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace gpaucarS1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludo_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int edad=Convert.ToInt32(txtEdad.Text);
+            double sueldo = Convert.ToInt32(txtSalario.Text);
+            double iess = ((9.45) * sueldo)/100;
+            DisplayAlert("Bienvenido\n","Infomación afiliado\n" + nombre+" "+apellido+"\nEdad: "+edad+"\nSueldo mensual $: "+sueldo+"\nAportes IESS $: "+iess,"Cerrar");
+        } 
+
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace gpaucarS1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
